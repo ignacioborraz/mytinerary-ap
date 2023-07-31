e8088509e328aa95090c632c801994f7c42f6028
ignacioborraz <ignacioborraz@hotmail.com>
2023-07-31 11:23:05 -0300

    app

---
+++ src/main.jsx
@@ -0,0 +1,6 @@
+import ReactDOM from 'react-dom/client' //sintaxis de las importaciones
+//import QUECOSAMEQUIEROTRAER from './ruta o modulo'
+import App from './App.jsx'
+import './index.css'
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />)
---
+++ tailwind.config.js
@@ -0,0 +1,12 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    mode: 'jit',
+    purge: [
+        './src/**/*.{js,jsx,ts,tsx}'
+    ],
+    theme: {
+        extend: {}
+    },
+    variants: {},
+    plugins: []
+}
