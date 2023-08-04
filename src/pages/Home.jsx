64f801f84ca250d01c80c760a62ffd02556ec31d
ignacioborraz <ignacioborraz@hotmail.com>
2023-08-04 11:12:41 -0300

    effects

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,29 +1,24 @@
-import { useState } from "react";
+import { useState,useEffect } from "react";
 import Carousel from "../components/Carousel";
+import axios from "axios";
 
 export default function Home() {
 
   const [show,setShow] = useState(true)
+  const [data,setData] = useState([])
   const nombre1 = "My";
   const nombre2 = "Tinerary";
-  let data = [
-    { id: "am1", city: "Cancun", photo: "/img/america/cancun.jpg" },
-    { id: "am2", city: "New York", photo: "/img/america/newyork.jpg" },
-    { id: "am3", city: "Rio de Janeiro", photo: "/img/america/rioDeJaneiro.jpg"},
-    { id: "am4", city: "Ushuaia", photo: "/img/america/ushuaia.jpg" },
-    { id: "as1", city: "Bangkok", photo: "/img/asia/bangkok.jpg" },
-    { id: "as2", city: "Pekin", photo: "/img/asia/pekin.jpg" },
-    { id: "as3", city: "Singapur", photo: "/img/asia/singapur.jpg" },
-    { id: "as4", city: "Tokyo", photo: "/img/asia/tokio.jpg" },
-    { id: "eu1", city: "Ibiza", photo: "/img/europe/ibiza.jpg" },
-    { id: "eu2", city: "London", photo: "/img/europe/london.jpg" },
-    { id: "eu3", city: "Paris", photo: "/img/europe/paris.jpg" },
-    { id: "eu4", city: "Roma", photo: "/img/europe/roma.jpg" },
-    { id: "oc1", city: "Majuro", photo: "/img/oceania/majuro.jpg" },
-    { id: "oc2", city: "Sidney", photo: "/img/oceania/sidney.jpg" },
-    { id: "oc3", city: "Suva", photo: "/img/oceania/suva.jpg" },
-    { id: "oc4", city: "Wellington", photo: "/img/oceania/wellington.jpg" }
-  ];
+  useEffect(
+    ()=>{
+      axios('/data.json')
+        .then(res=>setData(res.data))
+        .catch(err=>console.log(err))
+    },        //callback que NO debe retornar nada y NO puede ser asincrona
+    []        //array de dependencias
+              //cuando está vacío EL EFECTO se ejecuta UNA UNICA VEZ cuando se monta el componente
+              //cuando tiene variables de dependencias EL EFECTO se ejecuta (cuando se monta y) CADA VEZ que varía/cambia alguna de esas variables
+    //[show]  //en este ejemplo CADA VEZ que el booleano CAMBIE, se ejecuta la caalback de la L28
+  )
 
   return (
     <main className="grow flex flex-col items-center mt-[20px]">
